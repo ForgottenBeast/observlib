60f6f08d074ba2d5efa0fffd90259e68858cb176
ForgottenBeast <dev@null.nl>
2025-03-18 16:41:46 +0100

    lint observlib

--- lib/observlib/client.py
+++ lib/observlib/client.py
@@ -5,13 +5,21 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.trace.span import format_trace_id
-from opentelemetry.sdk.trace.export import BatchSpanProcessor,
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 # global observer object
 OBSERVER = None
 
+
 class Observer:
-    def __init__(self, service_name, prometheus_gtw = None, pyroscope_server = None, otlp_server = None, prometheus_port = None):
+    def __init__(
+        self,
+        service_name,
+        prometheus_gtw=None,
+        pyroscope_server=None,
+        otlp_server=None,
+        prometheus_port=None,
+    ):
         self.prometheus_gtw = prometheus_gtw
         self.pyroscope_server = pyroscope_server
 
@@ -19,25 +27,26 @@ class Observer:
             self.otlp_server = otlp_server + "/v1/traces"
 
         if otlp_server:
-            resource = Resource(attributes = {
-            SERVICE_NAME: service_name
-            })
+            resource = Resource(attributes={SERVICE_NAME: service_name})
             tracerProvider = TracerProvider(resource=resource)
-            processor = BatchSpanProcessor(OTLPSpanExporter(endpoint = self.otlp_server))
+            processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=self.otlp_server))
             tracerProvider.add_span_processor(processor)
             trace.set_tracer_provider(tracerProvider)
             self.tracer = trace.get_tracer(__name__)
 
         if pyroscope_server:
-            pyroscope.configure(application_name = service_name,
-                                server_address = pyroscope_server)
-
+            pyroscope.configure(
+                application_name=service_name, server_address=pyroscope_server
+            )
 
         self.registry = CollectorRegistry()
         self.service_name = service_name
 
     def push_metrics(self):
-        push_to_gateway(self.prometheus_gtw, job = self.service_name, registry = self.registry)
+        push_to_gateway(
+            self.prometheus_gtw, job=self.service_name, registry=self.registry
+        )
+
 
 def get_trace_id(span):
     return format_trace_id(span.get_span_context().trace_id)
