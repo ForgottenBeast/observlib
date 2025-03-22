6a02b361f32a367a265e595492201ff002a258f0
ForgottenBeast <dev@null.nl>
2025-03-22 14:49:57 +0100

    deadnix update

--- nix/shells/dev/default.nix
+++ nix/shells/dev/default.nix
@@ -5,7 +5,6 @@
   # You also have access to your flake's inputs.
 
   # The namespace used for your flake, defaulting to "internal" if not set.
-  namespace,
 
   # All other arguments come from NixPkgs. You can use `pkgs` to pull shells or helpers
   # programmatically or you may add the named attributes as arguments here.
