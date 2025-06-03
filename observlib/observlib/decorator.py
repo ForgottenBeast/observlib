0441e6c46e8823bb7a7be4a896e2fad3aced5aa3
ForgottenBeast <dev@null.nl>
2025-06-03 19:17:26 +0200

    refactor the decorator to support full configs

--- observlib/observlib/decorator.py
+++ observlib/observlib/decorator.py
@@ -13,7 +13,36 @@ def traced(
     amount_fn=None,
     tracer=None,
 ):
+
     def decorator(func):
+        def record_data(func_name, timing_histogram, counter, counter_factory,label_fn, amount_fn, start_time,result, error):
+            if timing_histogram and callable(timer_factory):
+                if isinstance(timing_histogram, (str, bytes)):
+                    config = {"name": timing_histogram}
+                else:
+                    config = timing_histogram
+
+                exec_time_histogram = timer_factory(frozenset(config.items()))
+                exec_time_histogram.record(
+                    time.perf_counter() - start_time, attributes = {"function": func_name}
+                )
+
+            labels = (
+                label_fn(result=result, exception=error) if label_fn else {}
+            )
+            amount = (
+                amount_fn(result=result, exception=error) if amount_fn else 1
+            )
+
+            if counter and callable(counter_factory):
+                if isinstance(counter, (str, bytes)):
+                    config = {"name": counter}
+                else:
+                    config = counter
+
+                actual_counter = counter_factory(frozenset(config.items()))
+                actual_counter.add(amount, attributes=labels)
+
         @wraps(func)
         def sync_wrapper(*args, **kwargs):
             start = time.perf_counter()
@@ -28,32 +57,7 @@ def traced(
                     raise
 
                 finally:
-                    if timing_histogram and callable(timer_factory):
-                        if isinstance(timing_histogram, (str, bytes)):
-                            config = {"name": timing_histogram}
-                        else:
-                            config = timing_histogram
-
-                        exec_time_histogram = timer_factory(config)
-                        exec_time_histogram.record(
-                            time.perf_counter() - start, {"function": func.__name__}
-                        )
-
-                    labels = (
-                        label_fn(result=result, exception=error) if label_fn else {}
-                    )
-                    amount = (
-                        amount_fn(result=result, exception=error) if amount_fn else 1
-                    )
-
-                    if counter and callable(counter_factory):
-                        if isinstance(counter, (str, bytes)):
-                            config = {"name": counter}
-                        else:
-                            config = counter
-
-                        actual_counter = counter_factory(config)
-                        actual_counter.add(amount, attributes=labels)
+                    record_data(func.__name__, timing_histogram, counter, counter_factory,label_fn, amount_fn, start, result, error)
 
         @wraps(func)
         async def async_wrapper(*args, **kwargs):
@@ -70,32 +74,7 @@ def traced(
                     raise
 
                 finally:
-                    if timing_histogram and callable(timer_factory):
-                        if isinstance(timing_histogram, (str, bytes)):
-                            config = {"name": timing_histogram}
-                        else:
-                            config = timing_histogram
-
-                        exec_time_histogram = timer_factory(config)
-                        exec_time_histogram.record(
-                            time.perf_counter() - start, {"function": func.__name__}
-                        )
-
-                    labels = (
-                        label_fn(result=result, exception=error) if label_fn else {}
-                    )
-                    amount = (
-                        amount_fn(result=result, exception=error) if amount_fn else 1
-                    )
-
-                    if counter and callable(counter_factory):
-                        if isinstance(counter, (str, bytes)):
-                            config = {"name": counter}
-                        else:
-                            config = counter
-
-                        actual_counter = counter_factory(config)
-                        actual_counter.add(amount, attributes=labels)
+                    record_data(func.__name__, timing_histogram, counter, counter_factory,label_fn, amount_fn, start, result, error)
 
         if asyncio.iscoroutinefunction(func):
             return async_wrapper
